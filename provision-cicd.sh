163d6af6cf3d994ecabbfc7f23851c5b043f2104
Jijie Chen <jijie.chen@someplace.com>
2019-11-05 10:09:58 +0800

    更新了学员准备文档中的措辞

--- provision-cicd.sh
+++ provision-cicd.sh
@@ -90,6 +90,9 @@ create_ns(){
 #   sonarqube: 1.25G 2.5G
 #   nexus: 0.5G 2Gi
 function deploy() {
+    export DEPLOY_SUFFIX="$ARG_PROJECT_SUFFIX"
+    export DNS_SUFFIX=$(cat ./cicd-infra/vars | grep dns_suffix | cut -d '=' -f 2)
+    
     create_ns dev-$ARG_PROJECT_SUFFIX
     create_ns stage-$ARG_PROJECT_SUFFIX
     create_ns prod-$ARG_PROJECT_SUFFIX
@@ -128,9 +131,6 @@ function deploy() {
   
   kubectl logs pods/$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}' -l job-name=cicd-installer)
   echo "Installation completed."
-
-  export DEPLOY_SUFFIX="$ARG_PROJECT_SUFFIX"
-  export DNS_SUFFIX=$(cat ./cicd-infra/vars | grep dns_suffix | cut -d '=' -f 2)
 }
 
 function kcd() {
@@ -156,12 +156,12 @@ echo_header ".NET Core Workshop on Kubernetes ($(date))"
 case "$ARG_COMMAND" in
     delete)
         echo "Delete demo..."
+        export DEPLOY_SUFFIX=
+        export DNS_SUFFIX=
         kubectl delete namespace dev-$ARG_PROJECT_SUFFIX stage-$ARG_PROJECT_SUFFIX prod-$ARG_PROJECT_SUFFIX cicd-$ARG_PROJECT_SUFFIX
         echo
         echo "Delete completed successfully!"
         kcd default
-        export DEPLOY_SUFFIX=
-        export DNS_SUFFIX=
         ;;
       
     deploy)
